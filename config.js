e996f7d1260564ed1d2f4b316fd44c5c482f0066
Denish Maharjan <denish.freelancer@gmail.com>
2020-07-05 16:38:26 +0545

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook':{
+        clientId: '739052503592995' ,
+        clientSecret:'b21a785805b146d35497a3da37c1198b'
+    }
 }
